df8e35da7e12312551939db38770338a7276a413
aman yadav <yaman49751@gmail.com>
2024-03-23 09:56:00 +0700

    abstraction

---
+++ Abstraction/Abstract.java
@@ -0,0 +1,8 @@
+
+
+public class Abstract {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // in this example you dont have to focus on the interal details of println function you just have to focus on which thing you have to print that it.
+    }
+}
---
+++ Abstraction/Redme.md
@@ -0,0 +1,9 @@
+## Abstraction
+hiding the unnecessary details,  only showing required information
+
+to solve designing issue.
+it focues on external stuff
+
+it is achieved by using abstract class and interfaces
+
+abstraction is a gaining of information
